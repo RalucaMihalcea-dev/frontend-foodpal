9925a0b928c84465a3357f1a30ddb76d49918e7f
Mihalcea Raluca <raluca.mihalcea.e@gmail.com>
2021-03-09 19:02:43 +0200

    updates

--- src/app/order/models/catalogue-item.ts
+++ src/app/order/models/catalogue-item.ts
@@ -1,11 +1,6 @@
-import { CatalogueItemCategory } from './catalogue-item-category';
-
-  export class CatalogueItem {
-    id: number;
-    name: string;
-    price: number;
-    category?: CatalogueItemCategory;
-    
-    
-    
-  }
+export class CatalogueItem {
+  id: number;
+  name: string;
+  price: number;
+  category?: { id: number; name: string };
+}
